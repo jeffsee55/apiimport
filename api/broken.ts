f92ebab4ef053247f2fc3ef55ebcc44f0cdf6d1e
Jeff See <jeffsee.55@gmail.com>
2024-07-24 09:12:29 -0700

    Use .js extension

--- api/broken.ts
+++ api/broken.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { message } from "../lib/message";
+import { message } from "../lib/message.js";
 
 export default function handler(
   request: VercelRequest,
